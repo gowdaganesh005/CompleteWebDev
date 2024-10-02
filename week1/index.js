b4c52e02d7d8ed44c7a4b44a90def6063dc320ae
Ganesh gowda <gowdaganesh0511@gmail.com>
2024-10-02 10:29:35 +0530

    monogoose intro

--- week1/index.js
+++ week1/index.js
@@ -1,5 +1,25 @@
 const express=require('express');
 const jwt= require('jsonwebtoken');
+const mongoose=require('mongoose')
+
+mongoose.connect("mongodb+srv://gowda:gowda@cluster0.gmjudtm.mongodb.net/tutdb/tut1")
+
+const User=mongoose.model(
+    "Users",{
+       
+        username: String,
+        password: String,
+    }
+
+)
+// const user=new User({
+//     username:"rahul",
+//     email: "jkfak@jkdsl",
+//     password:"3472598475f"
+// })
+
+// user.save();
+console.log("END")
 
 const jwtKey="1234"
 
@@ -21,13 +41,12 @@ const users=[
     }
 ]
 
-function userExists(username,password){
+function userExists(username){
     
-    if(users.find(user=>user.username==username && user.password==password)){
-        console.log("found")
+    if(User.findOne({username: username})){
         return true;
     }
-    return false
+    return False
 }
 
 app.post('/signin',(req,res)=>{
@@ -49,19 +68,41 @@ app.post('/signin',(req,res)=>{
 
 })
 
-app.get("/users",(req,res)=>{
-    const token=req.headers.authorization;
-    const decoded=jwt.verify(token,jwtKey);
-    const username=decoded.username;
-    res.json({
-        users: users.filter(user=>{
-            if(user.username==username){
-                return false;
-            }
-            return true;
+app.post("/signup",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    if(userExists(username,password)){
+        console.log("User  exists")
+         res.status(400).json({msg:"User exists"});
+
+         const user=new User({
+            username:username,
+            
+            password:password,
         })
-    })
+        user.save();
+    }
+    
+
+    
+
+   
 })
+
+// app.get("/users",(req,res)=>{
+//     const token=req.headers.authorization;
+//     const decoded=jwt.verify(token,jwtKey);
+//     const username=decoded.username;
+//     res.json({
+//         users: users.filter(user=>{
+//             if(user.username==username){
+//                 return false;
+//             }
+//             return true;
+//         })
+//     })
+// })
 
 app.listen("8000",()=>{
     console.log("Server listening on port 8000")
