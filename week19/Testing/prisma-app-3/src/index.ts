f69b23a094a597d2b01b86b16fce051c5663b74f
Ganesh gowda <gowdaganesh0511@gmail.com>
2025-01-19 20:41:37 +0530

    adding mocking return values and spying the outgoing values to db

--- week19/Testing/prisma-app-3/src/index.ts
+++ week19/Testing/prisma-app-3/src/index.ts
@@ -9,11 +9,15 @@ app.post("/sum",async (req,res)=>{
     const ans = data.a + data.b;
     const response = await prismaClient.sum.create({
         data:{
-            sum:ans
+            sum:ans,
+            a:data.a,
+            b:data.b,
         }
     })
+    console.log(response.sumId)
     res.status(200).json({
-        ans:ans
+        ans:ans,
+        sumId:response.sumId
     })
 
 })
--- week19/Testing/prisma-app-3/src/tests/index.test.ts
+++ week19/Testing/prisma-app-3/src/tests/index.test.ts
@@ -1,7 +1,7 @@
 import { describe,expect,it,vi } from "vitest";
 import request from "supertest";
 import { app } from "..";
-import { prismaClient } from "../db";
+import { prismaClient } from "../__mocks__/db";
 
 // vi.mock('../db',()=>{
 //     return{
@@ -20,13 +20,39 @@ import { prismaClient } from "../db";
 vi.mock('../db')
 
 
+
+
 describe("Testing with mocking database",()=>{
     it("add 2 and 3",async ()=>{
+        
+        prismaClient.sum.create.mockResolvedValue({
+            sumId:1,
+            sum:5,
+            a:2,
+            b:3
+        }) /// in the main code the id must be returned by the actual value but we are here mocking the returned value to able to use it as reponse if the dev have retured the response.sum instead sumvalue so it makes testing the returnValues
+        // mocking the output of database call
+
+        vi.spyOn(prismaClient.sum,"create")   // NOTE: this must be called after mocking the return value else the funciton retuen create doesnt exists
+        // this spys on the actual vlaue that the developer is sending across the db making sure correct values are sent 
+        // thsi is what is spying is and actually helps when data is enterd correctly
+        
+
+
+
         const ans = await request(app).post("/sum").send({
             a:2,
             b:3
         })
+        expect(prismaClient.sum.create).toHaveBeenCalledWith({    // actual testing the values that are sent in main request
+            data:{
+                sum:5,
+                a:2,
+                b:3
+            }
+        })
         expect(ans.statusCode).toBe(200);
+        expect(ans.body.sumId).toBe(1)
         expect(ans.body.ans).toBe(5);
     })
 })
